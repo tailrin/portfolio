21f2d9fe0bf2ad93c4e7ccf3c28c48cefcc4e6a7
joshua hahn <tailrin.jh@gmail.com>
2020-04-22 21:58:17 -0600

    added projects

--- store.js
+++ store.js
@@ -6,7 +6,30 @@ const store =[
         repo: "https://github.com/tailrin/quiz-app",
         description: "This app is a simple quiz app of the state capitals. 15 of the 50 states will be randomly chosen to be asked about. This quiz would be great for kids who are studying United States geography.",
         tech: ["Javascript", "Jquery", "html", "css"]
-
+    },
+    {
+        screenShot: "brewery.png",
+        name: "Brewery App",
+        live: "https://tailrin.github.io/brewery-app/",
+        repo: "https://github.com/tailrin/brewery-app",
+        description: "Looking for a fun experience and a good bood in your area? This is the app for you. You can use this app to find a local brewery within a specified distance of a specified location.",
+        tech: ["Javascript", "Jquery", "html", "css"]
+    },
+    {
+        screenShot: "projectTracker.png",
+        name: "Project Tracker",
+        live: "https://project-tracker.now.sh/",
+        repo: "https://github.com/tailrin/project-tracker-client",
+        description: "This a a collaborative project tracker designed for use by companies. You can create and prioritize projects, track tasks and assign priority.",
+        tech: ["Javascript", "node", "postgre", "html", "css", "react"]
+    },
+    {
+        screenShot: "bugTrapper.png",
+        name: "Bug Trapper",
+        live: "https://bug-trapper-client.now.sh",
+        repo: "https://github.com/tailrin/bug-trapper-client",
+        description: "This app is an issue tracker for software development. You can track projects and and log issues as well as keep track of the work done on those issues",
+        tech: ["Javascript", "node", "postgre", "html", "css", "react"]
     }
 ]
 
